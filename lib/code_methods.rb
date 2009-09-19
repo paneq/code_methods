9cdab6a08b3af28147d081cf795da9664b0bfb79
Robert Pankowecki <robert.pankowecki@gmail.com>
2009-09-19 16:53:00 +0200

    First version with simple standalone tests.

--- lib/code_methods.rb
+++ lib/code_methods.rb
@@ -1,6 +1,63 @@
 $:.unshift(File.dirname(__FILE__)) unless
-  $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
+$:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
+#
+# Usage: Use when you want to have easy access to some dictionary
+#        that is stored in database via generated methods.
+#
+# class PaymentType < ActiveRecord::Base
+#   include CodeMethods.based_on(:code)
+# end
+#
+# Result:
+#
+# if your payment_types database table has column named 'code' and is populated with
+# three records whose codes are: 'ADJUSTMENT', 'MONTHLY_FEE' and 'PENALTY' then
+# such methods will be generated
+#
+# PaymentType.ADJUSTMENT
+# PaymentType.MONTHLY_FEE
+# PaymentType.PENALTY
+#
+# Calling method PaymentType.PENALTY is equal to calling
+# Payment.find_by_code('PENALTY')
+# 
 module CodeMethods
-  VERSION = '0.0.1'
+  VERSION = '1.0.0'
+
+  def self.column_name
+    @@c_name ||= 'code'
+    @@c_name
+  end
+
+  def self.column_name=(name)
+    @@c_name = name
+  end
+
+  def self.included(klass)
+    column = self.column_name
+
+    class_methods = Module.new do
+      method_name = "find_by_#{column}"
+
+      klass.find(:all).each do |active_record_obj|
+        code = active_record_obj.send(column)
+        define_method(code) do
+          send(method_name, code)
+        end
+      end
+
+    end
+
+    klass.extend(class_methods)
+  end
+
+  # include CodeMethods.based_on(:code)
+  def self.based_on(column_name)
+    clonned_module = self.clone
+    clonned_module.column_name = column_name
+    clonned_module
+  end
+
 end
+
--- test/test_code_methods.rb
+++ test/test_code_methods.rb
@@ -2,10 +2,48 @@ require File.dirname(__FILE__) + '/test_helper.rb'
 
 class TestCodeMethods < Test::Unit::TestCase
 
-  def setup
+  # Tested class that behaves a bit like ActiveRecord::Base
+  class Tested < Struct.new(:code, :type)
+
+    A_BOY = new(:BOY, :NAUGHTY)
+    A_GIRL = new(:GIRL,:CRAZY)
+
+    INSTANCES = [
+      A_BOY,
+      A_GIRL
+    ]
+
+    # simulates find(:all) call
+    def self.find(*params)
+      INSTANCES
+    end
+
+    def self.find_by_code(code)
+      INSTANCES.find{|i| i.code == code}
+    end
+
+    def self.find_by_type(type)
+      INSTANCES.find{|i| i.type == type}
+    end
+    
   end
   
-  def test_truth
-    assert true
+  def test_generating_methods
+    Tested.instance_eval do
+      include CodeMethods
+    end
+
+    assert_equal Tested::A_BOY, Tested.BOY()
+    assert_equal Tested::A_GIRL, Tested.GIRL()
+
+    Tested.instance_eval do
+      include CodeMethods.based_on(:type)
+    end
+
+    assert_equal Tested::A_BOY, Tested.NAUGHTY()
+    assert_equal Tested::A_GIRL, Tested.CRAZY()
+    assert_equal Tested::A_BOY, Tested.BOY()
+    assert_equal Tested::A_GIRL, Tested.GIRL()
   end
+
 end
